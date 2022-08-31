0bc115e6c9135abf20a6168e766205daaf4c6d40
Caleb Turner <turner632@morrisville.edu>
2022-08-31 12:23:56 -0400

    SerializeField

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    float fltxValue = 0.01f;
-    float fltyValue = 0.01f;
-    float fltzValue = 0f;
+    [SerializeField]float fltxValue = 0.01f;
+   [SerializeField] float fltyValue = 0.01f;
+   [SerializeField] float fltzValue = 0f;
     // Start is called before the first frame update
     void Start()
     {
