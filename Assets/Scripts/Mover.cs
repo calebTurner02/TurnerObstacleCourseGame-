8383368d1c9dc4f935a9bb6ca9740ce22464ba9d
Caleb Turner <turner632@morrisville.edu>
2022-08-31 13:45:17 -0400

    method

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -12,13 +12,25 @@ public class Mover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       
+       PrintInstruction();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float fltXValue = Input.GetAxis("Horizontal") * Time.deltaTime * fltMoveSpeed;
+      MovePlayer();
+    }
+
+    void PrintInstruction()
+    {
+        Debug.Log("Welcome!");
+        Debug.Log("Move with WASD");
+        Debug.Log("Don't hit walls");
+
+    }
+     void MovePlayer()
+    {
+         float fltXValue = Input.GetAxis("Horizontal") * Time.deltaTime * fltMoveSpeed;
         float fltZValue = Input.GetAxis("Vertical") * Time.deltaTime * fltMoveSpeed;
         transform.Translate(fltXValue,0,fltZValue);
     }
